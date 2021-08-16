eeb7a0e0008498de809f464ba15b25f9baaf66c1
rasp0wn <68296250+rasp0wn@users.noreply.github.com>
2021-08-16 13:26:17 +0200

    Update install_wifi_drivers.sh

--- install_wifi_drivers.sh
+++ install_wifi_drivers.sh
@@ -1,11 +1,21 @@
 # Steps from https://github.com/morrownr/8812au
+sudo apt-get install raspberrypi-kernel-headers
+sudo apt-get install dkms
 
-sudo apt update
-sudo apt install -y raspberrypi-kernel-headers bc build-essential dkms git
-mkdir ~/src
-cd ~/src
-git clone https://github.com/morrownr/8812au.git
-cd ~/src/8812au
-./raspi32.sh
-sudo ./install-driver.sh
-sudo reboot
+git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
+cd rtl*
+sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
+sed -i 's/CONFIG_PLATFORM_ARM_RPI = n/CONFIG_PLATFORM_ARM_RPI = y/g' Makefile
+sudo make dkms_install
+
+
+# Old method
+#sudo apt update
+#sudo apt install -y raspberrypi-kernel-headers bc build-essential dkms git
+#mkdir ~/src
+#cd ~/src
+#git clone https://github.com/morrownr/8812au.git
+#cd ~/src/8812au
+#./raspi32.sh
+#sudo ./install-driver.sh
+#sudo reboot
